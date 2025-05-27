dd4f7ef5ccc41a1960cd4cde5040f09f503f2429
sashadieryckx <sashadieryckx@hotmail.com>
2025-05-27 13:47:54 -0400

    grants & partners update

--- src/views/about/GrantsAndPartnersView.vue
+++ src/views/about/GrantsAndPartnersView.vue
@@ -1,16 +1,21 @@
 <template>
   <div id="main-content" class="main-content df-pad">
-    <h2>Grants and Partners</h2>
-    <p>
-      The Beug Lab is grateful for the support of our funding partners and collaborators. We
-      acknowledge the following grants and partners that have contributed to our research:
-    </p>
-    <ul>
-      <li><strong>Canadian Institutes of Health Research (CIHR)</strong> - Operating Grant</li>
-      <li><strong>Genome Canada</strong> - Genomics Innovation Network</li>
-      <li><strong>Ontario Research Fund</strong> - Research Excellence Program</li>
-      <li><strong>CHEO Research Institute</strong> - Collaborative Research Initiatives</li>
-    </ul>
+    <div id="affiliations-section">
+      <h2>Affiliations</h2>
+      <div id="affiliation-links" class="clickable">
+        <a href="https://www.cheoresearch.ca/">Cheo Research Intitute</a>
+        <a href="https://www.uottawa.ca/faculty-medicine/biochemistry-microbiology-immunology"
+          >University of Ottawa Faculty of Medicine</a
+        >
+        <a href="https://www.uottawa.ca/faculty-medicine/center-infection-immunity-inflammation"
+          >Centre for Infection, Immunity and Inflammation</a
+        >
+        <a
+          href="https://www.chairs-chaires.gc.ca/chairholders-titulaires/profile-eng.aspx?profileId=4791"
+          >Canada Research Chair</a
+        >
+      </div>
+    </div>
     <div id="support-section">
       <h2>Supported By</h2>
       <div id="logos-container">
@@ -44,29 +49,60 @@
   position: relative;
   min-height: 100vh;
 }
+#affiliation-links a {
+  color: var(--font-color-main);
+  text-decoration: none;
+  font-family: akzidenz-grotesk-next-pro, sans-serif;
+  font-weight: 300;
+  font-style: normal;
+  transition: all 0.5s ease;
+}
+#affiliation-links a:hover {
+  color: var(--font-color-hover);
+}
 /* DESKTOP 1 [GLOBAL] */
 @media (min-width: 1280px) {
   #main-content {
-    padding-top: 8em;
+    padding-top: 4em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
+  #affiliations-section {
+    padding-bottom: 8em;
+    width: 100%;
+  }
+  #affiliations-section h2 {
+    text-align: center;
+  }
+  #affiliation-links {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    gap: 4em;
+    padding-top: 1em;
+  }
+  #affiliation-links a {
+    font-size: 1.5em;
   }
   h2 {
     font-size: 4em;
-    border-bottom: 2px solid var(--nero);
   }
   #support-section h2 {
     font-size: 4em;
-    padding-top: 2em;
+    text-align: center;
   }
   #logos-container {
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    flex-wrap: wrap;
-    gap: 2em;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-row: repeat(2, 1fr);
+    gap: 6em;
     padding-top: 4em;
   }
   .inst-logo {
-    width: 20em;
+    width: 18em;
     height: auto;
   }
 }
