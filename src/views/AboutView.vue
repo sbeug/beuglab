e0e7d72366507a346f606c1481938f0f9b49f6e2
sashadieryckx <sashadieryckx@hotmail.com>
2025-06-18 15:00:24 -0400

    about page experience container coded

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,3 +1,87 @@
+<script setup>
+import { onMounted } from 'vue'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/ScrollTrigger'
+
+gsap.registerPlugin(ScrollTrigger)
+
+onMounted(() => {
+  gsap.from('#about-us', {
+    opacity: 0,
+    ease: 'power2.inOut',
+    delay: 0.5,
+    duration: 2,
+  })
+  const experienceContainerTl = gsap.timeline({
+    scrollTrigger: {
+      trigger: '#experience-container',
+      start: 'top 0%',
+      end: '+=500%',
+      pin: true,
+      pinSpacing: true,
+      scrub: true,
+      invalidateOnRefresh: true,
+    },
+  })
+  experienceContainerTl.to(
+    '#about-us',
+    {
+      opacity: 0,
+      ease: 'power2.inOut',
+      duration: 0.5,
+    },
+    0,
+  )
+  experienceContainerTl.from(
+    '#h-top',
+    {
+      opacity: 0,
+      ease: 'power2.inOut',
+      duration: 0.75,
+      xPercent: -150,
+    },
+    0.5,
+  )
+  experienceContainerTl.from(
+    '#h-bottom',
+    {
+      opacity: 0,
+      ease: 'power2.inOut',
+      duration: 0.75,
+      xPercent: 150,
+    },
+    0.5,
+  )
+  experienceContainerTl.to(
+    '.experience-heading',
+    {
+      opacity: 0,
+      ease: 'power2.inOut',
+      duration: 0.5,
+      scale: 3.5,
+    },
+    1.25,
+  )
+  experienceContainerTl.to(
+    '#experience-video',
+    {
+      opacity: 0,
+      ease: 'power2.inOut',
+      duration: 0.5,
+    },
+    1.5,
+  )
+  experienceContainerTl.to(
+    '#experience-container',
+    {
+      opacity: 0,
+      ease: 'power2.inOut',
+      duration: 0.5,
+    },
+    1.5,
+  )
+})
+</script>
 <template>
   <div id="main-content">
     <div id="section-1">
@@ -5,7 +89,16 @@
         <video id="experience-video" autoplay muted playsinline loop preload="auto">
           <source src="../assets/content/video/BeugLab-ShowreelPreview.mp4" type="video/mp4" />
         </video>
-        <div class="experience-content">
+        <div id="about-us">
+          <h6>About us.</h6>
+          <h6>
+            We are interested in cancer biology with respect to understanding cell death mechanisms
+            and immunity. In particular, we focus on a group of proteins called the Inhibitor of
+            Apoptosis. We investigate how this group of proteins shape cancer development and
+            regulate immune responses.
+          </h6>
+        </div>
+        <div class="experience-heading">
           <div id="h-top">
             <h1>We are</h1>
           </div>
@@ -27,24 +120,15 @@
 #section-1 {
   position: relative;
   width: 100vw;
-  height: 100vh;
+  height: 500vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
 }
-#section-1-heading {
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  height: 100%;
-}
-#section-1-heading h1 {
-  font-size: 8em;
-  color: #181818;
-}
 #experience-container {
-  position: relative;
+  position: absolute;
+  top: 0;
   width: 100vw;
   height: 100vh;
   display: flex;
@@ -58,10 +142,24 @@
   width: 100%;
   height: 100%;
   object-fit: cover;
-  opacity: 75%;
+  opacity: 60%;
   scale: 1.05;
 }
-.experience-content {
+#about-us {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  width: 100%;
+  max-width: 75%;
+  color: var(--blanco);
+  text-align: justify;
+}
+#about-us :nth-child(1) {
+  font-size: 1em;
+  margin-bottom: 0.5em;
+}
+.experience-heading {
   position: absolute;
   top: 0;
   left: 0;
@@ -73,13 +171,23 @@
   justify-content: center;
   color: var(--blanco);
 }
-.experience-content h1 {
+.experience-heading h1 {
   font-size: 3em;
   text-transform: uppercase;
 }
 /* DESKTOP 1 [GLOBAL] */
 @media (min-width: 1280px) {
-  .experience-content h1 {
+  #about-us {
+    width: 100%;
+  }
+  #about-us :nth-child(1) {
+    font-size: 3em;
+  }
+  #about-us h6 {
+    font-size: 2.5em;
+    line-height: 1.5;
+  }
+  .experience-heading h1 {
     font-size: 14em;
   }
   #h-top,
